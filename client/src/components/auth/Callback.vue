cefb79965e8cb22dc3b12a2a2171ff5429016da3
CJ R <cj@null.computer>
2017-01-09 16:52:28 -0700

    :sparkles: Show login error message

--- client/src/components/auth/Callback.vue
+++ client/src/components/auth/Callback.vue
@@ -1,9 +1,16 @@
 <template>
-  <div class="center text-center">
-    <h1>Logging in...</h1>
-    <center>
-      <v-progress-circular active yellow yellow-flash></v-progress-circular>
-    </center>
+  <div>
+    <div class="center text-center" v-if="errorMessage">
+      <h1>Login Error</h1>
+      <p style="color:red">{{errorMessage}}</p>
+      <router-link to="/auth/login" class="waves btn">Try again</router-link>
+    </div>
+    <div class="center text-center" v-if="!errorMessage">
+      <h1>Logging in...</h1>
+      <center>
+        <v-progress-circular active yellow yellow-flash></v-progress-circular>
+      </center>
+    </div>
   </div>
 </template>
 
@@ -13,14 +20,20 @@ import Auth from '../../lib/Auth';
 export default {
   name: 'callback',
   data () {
-    return {}
+    return {
+      errorMessage: ''
+    }
   },
   created() {
-    Auth
-      .callbackLogin(this.$cookie)
-      .then(() => {
-        this.$router.push('/dashboard');
-      });
+    if(this.$route.params.error) {
+      this.errorMessage = this.$route.params.error;
+    } else {
+      Auth
+        .callbackLogin(this.$cookie)
+        .then(() => {
+          this.$router.push('/dashboard');
+        });
+    }
   }
 }
 </script>
