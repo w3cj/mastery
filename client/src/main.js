826fc5c6ba765c3511d56c65120a66b4ea86f554
CJ R <cj@null.computer>
2017-01-13 09:24:09 -0700

    :bookmark: Version 1.4.3

--- client/src/main.js
+++ client/src/main.js
@@ -2,8 +2,8 @@ import Vue from 'vue';
 import VueCookie from 'vue-cookie';
 import { sync } from 'vuex-router-sync';
 
-if(localStorage.version != '1.4.2') {
-  localStorage.version = '1.4.2';
+if(localStorage.version != '1.4.3') {
+  localStorage.version = '1.4.3';
   window.location.reload(true);
 }
 
