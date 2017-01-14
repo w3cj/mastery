b8800921121f751dd8ece8519a3a299cd1590be1
CJ R <cj@null.computer>
2017-01-14 16:19:07 -0700

    :bookmark: Version 1.5.0

--- client/src/main.js
+++ client/src/main.js
@@ -2,8 +2,8 @@ import Vue from 'vue';
 import VueCookie from 'vue-cookie';
 import { sync } from 'vuex-router-sync';
 
-if(localStorage.version != '1.4.4') {
-  localStorage.version = '1.4.4';
+if(localStorage.version != '1.5.0') {
+  localStorage.version = '1.5.0';
   window.location.reload(true);
 }
 
