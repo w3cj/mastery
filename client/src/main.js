76ed2606c120558359bb30b60a9383b67c5b0f13
CJ R <cj@null.computer>
2017-01-14 17:54:27 -0700

    :bookmark: Version 1.5.2

--- client/src/main.js
+++ client/src/main.js
@@ -2,8 +2,8 @@ import Vue from 'vue';
 import VueCookie from 'vue-cookie';
 import { sync } from 'vuex-router-sync';
 
-if(localStorage.version != '1.5.1') {
-  localStorage.version = '1.5.1';
+if(localStorage.version != '1.5.2') {
+  localStorage.version = '1.5.2';
   window.location.reload(true);
 }
 
