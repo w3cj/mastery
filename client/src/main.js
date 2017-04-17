115e9ca77b7b97972e0dad1bd86f465c82ba579f
CJ R <cj@null.computer>
2017-04-16 22:41:34 -0600

    :bookmark: Version 1.16.1

--- client/src/main.js
+++ client/src/main.js
@@ -6,8 +6,8 @@ import { sync } from 'vuex-router-sync';
 window.d3 = require('d3/d3.js');
 /* eslint-enable */
 
-if(localStorage.version != '1.16.0') {
-  localStorage.version = '1.16.0';
+if(localStorage.version != '1.16.1') {
+  localStorage.version = '1.16.1';
   window.location.reload(true);
 }
 
