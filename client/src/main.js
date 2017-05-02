a517ec7a8b328d6413acb556384e1e72d69f1146
CJ R <cj@null.computer>
2017-05-01 21:39:27 -0600

    :bookmark: Version 2.2.0

--- client/src/main.js
+++ client/src/main.js
@@ -5,8 +5,8 @@ import VueCookie from 'vue-cookie';
 window.d3 = require('d3/d3.js');
 /* eslint-enable */
 
-if(localStorage.version != '2.1.0') {
-  localStorage.version = '2.1.0';
+if(localStorage.version != '2.2.0') {
+  localStorage.version = '2.2.0';
   window.location.reload(true);
 }
 
