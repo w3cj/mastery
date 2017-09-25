4a75ec0324cf2ef90f8f00a5878aa7336123500c
CJ R <cj@null.computer>
2017-09-25 13:28:27 -0600

    :bookmark: Version 2.12.1

--- client/src/main.js
+++ client/src/main.js
@@ -10,8 +10,8 @@ if(!localStorage.reset && localStorage.defaultCohort == '162') {
   localStorage.reset = true;
 }
 
-if(localStorage.version != '2.12.0') {
-  localStorage.version = '2.12.0';
+if(localStorage.version != '2.12.1') {
+  localStorage.version = '2.12.1';
   window.location.reload(true);
 }
 
